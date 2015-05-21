f5b8cf8be4b89e1c1ad8a61af01a3ae78e44953b
k23Burke <k23burke@gmail.com>
2015-05-21 17:16:17 -0400

    first day

---
+++ public/app.js
@@ -0,0 +1,56 @@
+var app = angular.module('FlashCards', []);
+console.log(angular);
+
+app.controller('MainController', function($scope){
+	//blah
+	$scope.flashCards = [
+	    {
+	        question: 'What is Angular?',
+	        answers: [
+	            { text: 'A front-end framework for great power!', correct: true },
+	            { text: 'Something lame, who cares, whatever.', correct: false },
+	            { text: 'Some kind of fish, right?', correct: false }
+	        ]
+	    },
+	    {
+	        question: 'What is a controller?',
+	        answers: [
+	            { text: 'Something that manages my front-end routes', correct: false },
+	            { text: 'A function that allows me to manage a scope', correct: true },
+	            { text: 'An Angular template', correct: false }
+	        ]
+	    },
+	    {
+	        question: 'What does {{ }} do?',
+	        answers: [
+	            { text: 'It runs some Javascript', correct: false },
+	            { text: 'It looks for variables in HTML', correct: false },
+	            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+	        ]
+	    }
+	];
+});
+
+
+app.controller('FlashCardController', function($scope){
+	//blah
+	$scope.answered = false;
+	$scope.answeredCorrectly = false;
+
+	// $scope.flashCard = {
+	// 	question: 'What is Angular?',
+	// 	answers: [
+	// 		{ text: 'A front-end framework for great power!', correct: true },
+	//         { text: 'Something lame, who cares, whatever.', correct: false },
+	//         { text: 'Some kind of fish, right?', correct: false }
+ //        ]
+	// };
+
+	$scope.answerQuestion = function(answer) {
+		$scope.answered = true;
+		if(answer.correct) {
+			$scope.answeredCorrectly = true;
+		}
+		// console.log('click');
+	}
+});
